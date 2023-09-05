cff646878597c7fd4e64ee7b512f74d9ef89603e
Max Neuvians <max@neuvians.net>
2023-09-05 13:19:40 -0400

    feat: init (#2)
    * feat: init
    
    * fix: tag
    
    * feat: test json out
    
    * debug: url
    
    * fix: credentials
    
    * fix: credentials
    
    * debug: payload
    
    * debug: payload
    
    * fix: token
    
    * fix: token
    
    * fix: token
    
    * debug: url
    
    * debug: url
    
    * debug: payload
    
    * fix: clean up output
    
    * feat: format numbers
    
    * fix: test image
    
    * fix: update README
    
    * fix: update readme

--- README.md
+++ README.md
@@ -1,10 +1,66 @@
-# Generic Project Template
+# Dive action
 
-This repository provides some base files for setting up a repository at
-CDS. Plan is to create more project template for specific technologies:
+The purpose of this action is to run `wagoodman/dive` on a Docker image and posts the results as a comment on a PR in GitHub. It can also be used in other actions but will echo the JSON output to the CI/CD console instead. Using `dive` you can see which layers in your Docker image are taking up the most space and which ones are inefficient.
 
-- project-template-terraform
-- project-template-python
-- project-template-nodejs
+## Configuration
 
-Note that default community health files are maintained at https://github.com/cds-snc/.github 
+The action is configured using environment variables. The following variables are required:
+
+| Variable | Description |
+|--|--|
+| `GITHUB_TOKEN` | The GitHub token to use to post the comment. |
+| `IMAGE_NAME` | The name of the Docker image to run `dive` on. |
+
+The following variables are optional:
+
+| Variable | Description |
+|--|--|
+| `DIVE_VERSION` | The version of `dive` to use. Defaults to `0.11.0`. |
+
+## Example usage
+
+```yaml
+
+- name: Dive into docker image
+  uses: cds-snc/dive-action@main
+  env: 
+    GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+    IMAGE_NAME: "alpine:latest"
+```
+
+Just be sure to replace the `IMAGE_NAME` with the name of the image you want to run `dive` on. Most likely something you will have built in a previous step.
+
+## Example output
+
+### Dive image results for `alpine:latest`
+
+| | |
+| --- | --- |
+| Image | `alpine:latest` |
+| Total Size | `7.00MiB` |
+| Inefficient Bytes | `0.00B` |
+| Efficiency Percentage | `100.00%` |
+| Total Layers | `1` |
+
+<details>
+<summary>Show full output</summary>
+
+```json
+{
+  "layer": [
+    {
+      "index": 0,
+      "id": "b1a086cc7b4e637792beecd0316d55b301c5ac60d5988b0df9897b329616ac37",
+      "digestId": "sha256:4693057ce2364720d39e57e85a5b8e0bd9ac3573716237736d6470ec5b7b7230",
+      "sizeBytes": 7330497,
+      "command": "#(nop) ADD file:32ff5e7a78b890996ee4681cc0a26185d3e9acdb4eb1e2aaccb2411f922fed6b in / "
+    }
+  ],
+  "image": {
+    "sizeBytes": 7330497,
+    "inefficientBytes": 0,
+    "efficiencyScore": 1,
+    "fileReference": []
+  }
+}
+```
---
+++ run.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+set -e # Exit with nonzero exit code if anything fails
+
+BIN_DIR="${BIN_DIR:-/usr/local/bin}"
+RUNNER_TEMP="${RUNNER_TEMP:-/tmp}"
+
+# Check if we have an image name
+if [ -z "${IMAGE_NAME}" ]; then
+    echo "No image name found, exiting..."
+    exit 1
+fi
+
+# Set version
+DIVE_VERSION="${DIVE_VERSION:-0.11.0}"
+
+# Download and install dive
+rm -rf "${RUNNER_TEMP}/dive*"
+rm -f "${BIN_DIR}/dive"
+echo "Downloading dive ${DIVE_VERSION}..."
+wget "https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.tar.gz" -O "${RUNNER_TEMP}/dive_${DIVE_VERSION}_linux_amd64.tar.gz"
+wget "https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_checksums.txt" -O "${RUNNER_TEMP}/dive_checksums.txt"
+echo "$(grep "linux_amd64.tar.gz" "${RUNNER_TEMP}/dive_checksums.txt" | cut -d ' ' -f 1) ${RUNNER_TEMP}/dive_${DIVE_VERSION}_linux_amd64.tar.gz"  | sha256sum --check 
+tar -xzf "${RUNNER_TEMP}/dive_${DIVE_VERSION}_linux_amd64.tar.gz" -C "${RUNNER_TEMP}"
+chmod +x "${RUNNER_TEMP}/dive"
+mv "${RUNNER_TEMP}/dive" "${BIN_DIR}"
+rm "${RUNNER_TEMP}/dive_${DIVE_VERSION}_linux_amd64.tar.gz" "${RUNNER_TEMP}/dive_checksums.txt"
+echo "dive ${DIVE_VERSION} installed to ${BIN_DIR}/dive"
+
+# Run dive
+echo "Running dive on ${IMAGE_NAME} ..."
+dive "${IMAGE_NAME}" -j "${RUNNER_TEMP}/dive_output.json"
+
+# Assign results to env vars
+DIVE_IMAGE_NAME="${IMAGE_NAME}"
+DIVE_IMAGE_TOTAL_SIZE="$(jq -r '.image.sizeBytes' "${RUNNER_TEMP}/dive_output.json" | numfmt --to=iec-i --suffix=B --format="%.2f")"
+DIVE_IMAGE_INEFFICIENT_BYTES="$(jq -r '.image.inefficientBytes' "${RUNNER_TEMP}/dive_output.json" | numfmt --to=iec-i --suffix=B --format="%.2f")" 
+DIVE_IMAGE_EFFICIENCY_SCORE="$(jq -r '.image.efficiencyScore' "${RUNNER_TEMP}/dive_output.json")"
+DIVE_IMAGE_EFFICIENCY_PERCENT=$(awk -v num="$DIVE_IMAGE_EFFICIENCY_SCORE" 'BEGIN { printf "%.2f%%\n", num * 100 }')
+DIVE_IMAGE_TOTAL_LAYERS="$(jq -r '.layer | length' "${RUNNER_TEMP}/dive_output.json")"
+DIVE_IMAGE_JSON="$(cat "${RUNNER_TEMP}/dive_output.json")"
+
+# Export env vars
+export DIVE_IMAGE_NAME
+export DIVE_IMAGE_TOTAL_SIZE
+export DIVE_IMAGE_INEFFICIENT_BYTES
+export DIVE_IMAGE_EFFICIENCY_PERCENT
+export DIVE_IMAGE_TOTAL_LAYERS
+export DIVE_IMAGE_JSON
+
+# envsubst template
+TEMPLATE="$(envsubst < ./template.md)"
+
+# Post results to GitHub if this is a pull request 
+if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
+  # Check if we have a GitHub token
+    if [ -z "${GITHUB_TOKEN}" ]; then
+        echo "No GitHub token found, exiting..."
+        exit 1
+    fi
+  echo "Posting results to GitHub..."
+  PAYLOAD="$(echo "${TEMPLATE}" | jq -R --slurp '{body: .}')"
+  ISSUE_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+  URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${ISSUE_NUMBER}/comments"
+  echo "${PAYLOAD}" | curl -s -S -H "Authorization: Bearer ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data @- "${URL}" > /dev/null
+
+else
+    echo "Not a pull request, skipping posting results to GitHub."
+    # Echo JSON to stdout
+    echo "$DIVE_IMAGE_JSON"
+fi
