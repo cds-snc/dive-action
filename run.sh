52a06c4c74e72cb9804c2c17041c05e159adc3df
Max Neuvians <max@neuvians.net>
2023-09-05 15:15:08 -0400

    feat: merge template into sh (#4)
    * feat: merge template into sh
    
    * fix: debug

--- run.sh
+++ run.sh
@@ -48,7 +48,22 @@ export DIVE_IMAGE_TOTAL_LAYERS
 export DIVE_IMAGE_JSON
 
 # envsubst template
-TEMPLATE="$(envsubst < ./template.md)"
+TEMPLATE="### Dive image results for \`${DIVE_IMAGE_NAME}\`
+
+| | |
+| --- | --- |
+| Image | \`${DIVE_IMAGE_NAME}\` |
+| Total Size | \`${DIVE_IMAGE_TOTAL_SIZE}\` |
+| Inefficient Bytes | \`${DIVE_IMAGE_INEFFICIENT_BYTES}\` |
+| Efficiency Percentage | \`${DIVE_IMAGE_EFFICIENCY_PERCENT}\` |
+| Total Layers | \`${DIVE_IMAGE_TOTAL_LAYERS}\` |
+
+<details>
+<summary>Show full output</summary>
+
+\`\`\`json
+${DIVE_IMAGE_JSON}
+\`\`\`"
 
 # Post results to GitHub if this is a pull request 
 if [ "${GITHUB_EVENT_NAME}" == "pull_request" ]; then
